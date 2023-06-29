857a4f4df262dfd97ad843bca32bf8000db94e9f
Scott Smiesko <scott.smiesko@gmail.com>
2023-06-29 11:43:12 -0400

    morning commit
    * Require `solnlib` for prebuilt QoL features like KVStore checkpointing.
    * include simple fastapi server to emulate Generic EDR responses.

---
+++ bin/other/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI()
+
+@app.head("/")
+async def root():
+    pass
+
+@app.post("/api/v1/authenticate/simple")
+async def auth():
+    return {"secret": "1f10d113-e9e7-4174-b6e1-f8de66da7d1c", "expiry": str(datetime.utcnow())}
